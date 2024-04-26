4b521088fe7703f4990004a1e270f800a801e328
Morgan Senechal <morgan@neo4j.com>
2024-04-26 22:48:39 +0100

    Changed to read_only + listing folders&files for each buckets

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def authenticate_with_oauth(scopes):
     # your json config from google oauth
     client_secrets_file = 'config.json'
 
+    # this will open a browser window to authenticate the user
+    # you will need to change this auth flow to reflect your architecture (backend only/frontend+backend/ etc. )
     flow = InstalledAppFlow.from_client_secrets_file(
         client_secrets_file,
         scopes=scopes
@@ -12,22 +14,31 @@ def authenticate_with_oauth(scopes):
     creds = flow.run_local_server(port=0)
     return creds
 
-def list_gcs_buckets(project_id, credentials):
-    storage_client = storage.Client(project=project_id, credentials=credentials)
+def list_gcs_buckets(storage_client):
     buckets = storage_client.list_buckets()
     return buckets
 
+def list_files_in_bucket(bucket):
+    blobs = bucket.list_blobs()
+    return blobs
+
 def main():
-    scopes = ['https://www.googleapis.com/auth/devstorage.read_write']
+    scopes = ['https://www.googleapis.com/auth/devstorage.read_only']
     creds = authenticate_with_oauth(scopes)
 
     # here you would need to get user's projectid from the frontend
     project_id = 'ssomorgan'
 
-
-    buckets = list_gcs_buckets(project_id, creds)
+    storage_client = storage.Client(project=project_id, credentials=creds)
+    buckets = list_gcs_buckets(storage_client)
     for bucket in buckets:
-        print(bucket.name)
+        print('-------------------')
+        print('Bucket: ' + bucket.name)
+        bucket = storage_client.get_bucket(bucket.name)
+        blobs = list_files_in_bucket(bucket)
+        print('Files/Folders:')
+        for blob in blobs:
+            print(blob.name)
 
 if __name__ == '__main__':
     main()
