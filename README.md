e48da5fe704be8294f50feba155c03b874a34e91
Morgan Senechal <morgan@neo4j.com>
2024-05-07 16:06:34 +0100

    Updated to use fastAPI to show how to handle project-id in frontend

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # google-gcs-oauth
 google-gcs-oauth
 
-Create config.json based on the config.json.example, or download it directly from your google oauth app
+Create .env based on your google oauth app
 
 ```shell
 pip install -r requirements.txt
 python main.py
 ```
-
-+ The project ID needs to be set from the frontend and passed to the backend as the way you fetch gcs buckets is by projects
--- main.py
+++ main.py
@@ -1,44 +1,91 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import RedirectResponse, HTMLResponse
+from starlette.middleware.sessions import SessionMiddleware
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
 from google.cloud import storage
+from google.api_core.exceptions import GoogleAPIError, Forbidden
+import os
+from dotenv import load_dotenv
 
-def authenticate_with_oauth(scopes):
-    # your json config from google oauth
-    client_secrets_file = 'config.json'
+load_dotenv()
 
-    # this will open a browser window to authenticate the user
-    # you will need to change this auth flow to reflect your architecture (backend only/frontend+backend/ etc. )
-    flow = InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file,
-        scopes=scopes
-    )
-    creds = flow.run_local_server(port=0)
-    return creds
+# Those are the LLM Graph Builder Google Oauth APP ! Not the user !
+GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
+REDIRECT_URI = 'http://localhost:8000/oauth2callback'
 
-def list_gcs_buckets(storage_client):
-    buckets = storage_client.list_buckets()
-    return buckets
+SCOPES = ['https://www.googleapis.com/auth/devstorage.read_only']
 
-def list_files_in_bucket(bucket):
-    blobs = bucket.list_blobs()
-    return blobs
+app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key=os.urandom(24))
 
-def main():
-    scopes = ['https://www.googleapis.com/auth/devstorage.read_only']
-    creds = authenticate_with_oauth(scopes)
+oauth_config = {
+    "web": {
+        "client_id": GOOGLE_CLIENT_ID,
+        "client_secret": GOOGLE_CLIENT_SECRET,
+        "redirect_uris": [REDIRECT_URI],
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token"
+    }
+}
 
-    # here you would need to get user's projectid from the frontend
-    project_id = 'ssomorgan'
+@app.get('/')
+async def index():
+    return HTMLResponse('''<h1>GCS bucket demo</h1>
+                        <form action="/set_project" method="post">
+                            <label>GCP Project ID: </label>
+                            <input type="text" name="project_id" required>
+                            <input type="submit" value="Submit">
+                        </form>''')
 
-    storage_client = storage.Client(project=project_id, credentials=creds)
-    buckets = list_gcs_buckets(storage_client)
-    for bucket in buckets:
-        print('-------------------')
-        print('Bucket: ' + bucket.name)
-        bucket = storage_client.get_bucket(bucket.name)
-        blobs = list_files_in_bucket(bucket)
-        print('Files/Folders:')
-        for blob in blobs:
-            print(blob.name)
+@app.post('/set_project')
+async def set_project(request: Request, project_id: str = Form(...)):
+    request.session['project_id'] = project_id
+    return RedirectResponse(url='/authorize', status_code=303)
+
+@app.get('/authorize')
+async def authorize(request: Request):
+    if 'project_id' not in request.session:
+        return RedirectResponse(url='/')
+    flow = google_auth_oauthlib.flow.Flow.from_client_config(oauth_config, scopes=SCOPES)
+    flow.redirect_uri = REDIRECT_URI
+    authorization_url, state = flow.authorization_url(access_type='offline', include_granted_scopes='true')
+    request.session['state'] = state
+    return RedirectResponse(url=authorization_url)
+
+@app.get('/oauth2callback')
+async def oauth2callback(request: Request):
+    if 'project_id' not in request.session:
+        return RedirectResponse(url='/')
+    state = request.session['state']
+    flow = google_auth_oauthlib.flow.Flow.from_client_config(oauth_config, scopes=SCOPES, state=state)
+    flow.redirect_uri = REDIRECT_URI
+    flow.fetch_token(authorization_response=str(request.url))
+    request.session['credentials'] = credentials_to_dict(flow.credentials)
+    return RedirectResponse(url='/list_buckets')
+
+@app.get('/list_buckets')
+async def list_buckets(request: Request):
+    if 'credentials' not in request.session or 'project_id' not in request.session:
+        return RedirectResponse(url='/')
+    try:
+        storage_client = storage.Client(credentials=google.oauth2.credentials.Credentials(**request.session['credentials']), project=request.session['project_id'])
+        bucket_list = '<h1>List of Buckets</h1>' + ''.join([f'<p>Bucket: {bucket.name}</p>' for bucket in storage_client.list_buckets()])
+        return HTMLResponse(bucket_list)
+    except Forbidden as e:
+        error_message = f"<h1>Access to the bucket denied</h1><p>{e.message}</p>"
+        return HTMLResponse(error_message)
+    except GoogleAPIError as e:
+        error_message = f"<h1>Error: </h1><p>{e.message}</p>"
+        return HTMLResponse(error_message)
+
+def credentials_to_dict(credentials):
+    return {'token': credentials.token, 'refresh_token': credentials.refresh_token, 'token_uri': credentials.token_uri, 'client_id': credentials.client_id, 'client_secret': credentials.client_secret, 'scopes': credentials.scopes}
 
 if __name__ == '__main__':
-    main()
+    import uvicorn
+
+    # This is only for DEV ! (to be able to have oauth running on http instead of https), remove this in production!!
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    uvicorn.run(app, host='localhost', port=8000)
